2fb7422499f56b183939280c6a00f8abdb9ab5ba
Ivan L M Ricarte <ricarte@unicamp.br>
2015-02-06 15:26:13 -0200

    File to token
    Replace argument from local file by token to directly get the data from
    REDCap

--- getParticipantes.R
+++ getParticipantes.R
@@ -2,20 +2,18 @@
 #' 
 #' REDCap exporta dois arquivos para serem utilizados em R, um script R (.r) e um 
 #' arquivo de dados (.csv), que devem estar no diretorio de trabalho
-#' @param dir diretorio de trabalho
-#' @param limpo valor logico, importacao deve ou nao remover do conjunto de dados as observacoes incompletas
+#' @param token string do token para autorizar exportacao de dados na plataforma REDCap
 #' @return data.frame com dados importados
 #' @author Ivan L M Ricarte
 #' @export
-getParticipantes <- function(dir='.', limpo=FALSE) {
-    setwd(dir)
-    datafile <- list.files(pattern="EvidSPParticipantes.*.r")
-    if (length(datafile) == 0)
-        stop("Arquivos com dados de participantes nao encontrados em ", dir)
-    source(datafile[1], encoding='UTF-8-BOM')
-    if (limpo == TRUE)
-        data <- subset(data,redcap_event_name=="inscricao_arm_1" & 
-                tcle_concord==1 & 
-                evidsp_tcle_demogrfico_inicial_timestamp != '[not completed]')
-    data
+getParticipantes <- function(myToken) {
+    # token mandatorio
+    if (length(myToken) == 0)
+        stop("REDCap token is required")
+    
+    # obter dados exportados diretamente de REDCap
+    require(RCurl)
+    url <- "http://redcap.fmrp.usp.br/api/"
+    out <- postForm(url, token = myToken, content="record", format="csv", type="flat")
+    read.csv(textConnection(out),encoding="UTF-8-BOM")
 }
